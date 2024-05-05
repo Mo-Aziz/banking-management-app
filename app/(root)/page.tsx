b1f57654a1a6f8f553d02bfd71ca8e316518c116
Mo-Aziz <mohamed.aref32@gmail.com>
2024-05-05 06:30:25 -0500

    implement  the messaging box dynamically, chart js for the banking account section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const home = () => {
-  return <div> home</div>;
+  const loggedIn = { firstName: "Maximilian" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.95}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default home;
